216f595c0afb14f935b7855f58565005199161ae
flerpo <alex.rasten@gmail.com>
2018-10-08 18:58:24 +0200

    initial

---
+++ Competition/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class Tracks(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="images/tracks")
+
+    def __str__(self):
+        return self.name + ' ' + self.location
+
+
+class Results(models.Model):
+    user = models.CharField(max_length=30)
+    registered_time = models.CharField(max_length=20)
+    updated_time = models.DateTimeField()
+    user_image = models.ImageField(upload_to="images/userImages")
