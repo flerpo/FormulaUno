f3ef7e4ff054632122c5c4768144eb6e0236ca31
flerpo <alex.rasten@gmail.com>
2018-11-04 20:14:32 +0100

    more stuff

--- Competition/views.py
+++ Competition/views.py
@@ -1,3 +1,9 @@
+from django.http import request
 from django.shortcuts import render
-
 # Create your views here.
+from Competition.models import Tracks
+
+
+def tracks(request):
+    return render(request, 'index.html', {'tracks': Tracks.objects.all()})
+
--- FormulaUno/urls.py
+++ FormulaUno/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
+
+from Competition import views
+from FormulaUno import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tracks/', views.tracks, name="tracks"),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
